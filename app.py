2604c298756ac19b3b8a14e2308edc0d34225c5c
Camilo Castillo <horuxjcc@gmail.com>
2023-08-13 17:04:07 -0500

    ultima actualizacion

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
 from flaskext.mysql import MySQL
 from datetime import datetime
 from flask import send_from_directory
 import os
 
 app = Flask(__name__)
+app.secret_key = "digitalforge"
 
 mysql = MySQL()
 
@@ -24,6 +25,10 @@ def imagenes(imagen):
     print(imagen)
     return send_from_directory(os.path.join('templates/sitio/img'),imagen)
 
+@app.route('/css/<archivocss>')
+def cssLink(archivocss):
+    return send_from_directory(os.path.join('templates/sitio/css'),archivocss)
+
 @app.route('/libros')
 def libros():
     
@@ -41,14 +46,40 @@ def nosotros():
 
 @app.route('/admin/')
 def admin_index():
+    
+    if not 'login' in session:
+        return redirect('/admin/login')
+    
     return render_template('admin/index.html')
 
 @app.route('/admin/login')
 def adminLogin():
     return render_template('admin/login.html')
 
+@app.route('/admin/login', methods=['POST'])
+def adminLoginPost():
+    _usuario = request.form['txtUsuario']
+    _password = request.form['txtPassword']
+
+    if _usuario == "admin" and _password == "1234":
+        session["login"] = True
+        session["usuario"] = "Administrador"
+        return redirect("/admin")
+    
+    return render_template("admin/login.html", mensaje = "Acceso denegado")
+
+@app.route('/admin/cerrar')
+def adminLoginCerrar():
+    session.clear()
+    return redirect('/admin/login')
+
+
 @app.route('/admin/libros')
 def adminLibros():
+
+    if not 'login' in session:
+        return redirect('/admin/login')
+    
     conexion = mysql.connect()
     cursor = conexion.cursor()
     cursor.execute("SELECT * FROM libros")
@@ -59,6 +90,9 @@ def adminLibros():
 
 @app.route('/admin/libros/guardar', methods=['POST'])
 def adminLibrosGuardar():
+
+    if not 'login' in session:
+        return redirect('/admin/login')
 
     _nombre =  request.form['txtNombre']
     _url = request.form['txtURL']
@@ -83,6 +117,10 @@ def adminLibrosGuardar():
 
 @app.route('/admin/libros/borrar', methods=['POST'])
 def adminLibrosBorrar():
+
+    if not 'login' in session:
+        return redirect('/admin/login')
+    
     _id = request.form['txtID']
     print(_id)
 
---
+++ database/sitio.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-08-2023 a las 02:20:04
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sitio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libros`
+--
+
+CREATE TABLE `libros` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `imagen` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`id`, `nombre`, `imagen`, `url`) VALUES
+(3, 'C++', '2023201516_CPlusPlusGrow.png', 'https//:android-studio.com'),
+(6, 'Android', '2023203404_AndroidGrow.png', 'https://books.goalkicker.com/AndroidBook/AndroidNotesForProfessionals.pdf');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `libros`
+--
+ALTER TABLE `libros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
